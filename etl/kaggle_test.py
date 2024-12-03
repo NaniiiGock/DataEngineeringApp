d356a6df816c58b0f78be0bcc8bbed84b77a73ee
nikopensius <gustavnikopensius@gmail.com>
2024-12-03 13:29:21 +0200

    Change directory a bit

--- etl/kaggle_test.py
+++ etl/kaggle_test.py
@@ -1,8 +1,17 @@
-import kagglehub
+import kaggle.cli
+import sys
 import pandas as pd
+from pathlib import Path
+from zipfile import ZipFile
 
-# Download latest version
-path = kagglehub.dataset_download("winston56/fortune-500-data-2021")
-df = pd.read_csv(path)
+# download data set
+# https://www.kaggle.com/unsdsn/world-happiness?select=2017.csv
+dataset = "winston56/fortune-500-data-2021/"
+sys.argv = [sys.argv[0]] + f"datasets download {dataset}".split(" ")
+kaggle.cli.main()
 
-print(df.head())
+zfile = ZipFile(f"{dataset.split('/')[1]}.zip")
+
+dfs = {f.filename:pd.read_csv(zfile.open(f)) for f in zfile.infolist() }
+
+print(dfs['Fortune_1000.csv'].head())
