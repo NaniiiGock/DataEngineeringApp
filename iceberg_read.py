aef180938747415ae8203a56feb6c0139940da9c
Liliana <91615473+NaniiiGock@users.noreply.github.com>
2024-12-10 17:21:25 +0200

    Create iceberg_read.py

---
+++ iceberg_read.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("Query Iceberg Table") \
+    .config("spark.sql.catalog.local", "org.apache.iceberg.spark.SparkCatalog") \
+    .config("spark.sql.catalog.local.type", "hadoop") \
+    .config("spark.sql.catalog.local.warehouse", "./iceberg_warehouse") \
+    .config("spark.jars", "jars/iceberg-spark-runtime-3.5_2.12-1.7.1.jar") \
+    .getOrCreate()
+
+spark.sql("SELECT * FROM local.default.weather_data").show()
+
+spark.stop()
